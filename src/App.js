3891bd4e79dd499800bb22ba9cf22632bfd323fc
Ranjan MP <ranjanmp@Ranjans-MacBook-Air.local>
2019-10-06 23:54:47 +0530

    Added Button Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ButtonComponent from './components/button_component'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+    state = {  }
+    render() { 
+        return ( <div>
+            <ButtonComponent />
+        </div> );
+    }
 }
-
+ 
 export default App;
+
---
+++ src/components/button_component.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class ButtonComponent extends Component {
+    state = {  }
+    render() { 
+        return ( <div>          
+            <button className="btn btn-primary">Button</button>
+        </div> );
+    }
+}
+ 
+export default ButtonComponent;
